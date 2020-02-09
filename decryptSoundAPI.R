af4473af3816b5893b432b8c409644fbed38327f
ThiagoNato <45976848+ThiagoNato@users.noreply.github.com>
2020-02-09 13:46:09 -0300

    Project Initiation

---
+++ decryptSoundAPI.R
@@ -0,0 +1,19 @@
+library(decryptrSound)
+library(base64enc)
+
+#* @get /
+root <- function() {
+  "hello world"
+}
+
+#* Return text of capcha image
+#* @param encode64 MP3 in Base64
+#* @param guid Name of file
+#* @post /cpomPMSP
+cpomPMSP <- function(encode64, guid) {
+  # recebe o MP3 em base 64
+  # converte em um arquivo e salva na maquina
+  # carrega o captcha
+  # decrypta o captcha carregado
+  decryptSound(encode64, guid)
+}
